460f6944a3111c82af0f880abfe44e05be2028cf
赵举飞 <zjf@choicesoft.com.cn>
2021-01-25 16:27:42 +0800

    1

---
+++ example-practice/src/main/java/com/zhaojufei/bizline/example/practice/IntTest.java
@@ -0,0 +1,12 @@
+package com.zhaojufei.bizline.example.practice;
+
+public class IntTest {
+    public static void main(String[] args) {
+        int i =1;
+        System.out.println(i++);
+
+        i = i++;
+
+        System.out.println(i);
+    }
+}
